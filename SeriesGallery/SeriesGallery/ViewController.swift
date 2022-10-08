57e609dc37e44295e2f50bb7eb2bc9170176d221
jullynobre <jullynobre1@gmail.com>
2022-10-08 11:32:22 -0300

    Add xcode project

---
+++ SeriesGallery/SeriesGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeriesGallery
+//
+//  Created by Jully Nobre da Silva on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeriesGallery/SeriesGalleryUITests/SeriesGalleryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SeriesGalleryUITestsLaunchTests.swift
+//  SeriesGalleryUITests
+//
+//  Created by Jully Nobre da Silva on 08/10/22.
+//
+
+import XCTest
+
+class SeriesGalleryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
